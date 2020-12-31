faf6e57d95b6b4dc6e60b59203fa9a224344eebf
Ian Denhardt <ian@zenhack.net>
2020-12-31 14:42:41 -0500

    Avoid implementing map* in terms of flatten().
    The dependency graphs for our map() functions should all be static. This
    patch defines map2 as primitive, and defines the other map* functions in
    terms of that, rather than using .then() to implement mapN for N > 1.

--- inc.ts
+++ inc.ts
@@ -29,11 +29,7 @@ export abstract class Incr<T> {
   }
 
   map<A>(f: (v: T) => A): Incr<A> {
-    return new Map(this._reactor, this, f);
-  }
-
-  apply<A>(f: Incr<(v: T) => A>): Incr<A> {
-    return f.then<A>(f => this.map(f))
+    return map(this, f);
   }
 
   _subscribe(sub: Incr<any>) {
@@ -87,24 +83,21 @@ export abstract class Incr<T> {
   }
 }
 
-export function apply<A, B>(f: Incr<(v: A) => B>, x: Incr<A>): Incr<B> {
-  return x.apply(f)
-}
-
 export function flatten<A>(x: Incr<Incr<A>>): Incr<A> {
   return new Flatten(x._reactor, x);
 }
 
 export function map<A, B>(a: Incr<A>, fn: (a: A) => B): Incr<B> {
-  return a.map(fn);
+  return map2(a, a._reactor._constNull, (a, _) => fn(a));
 }
 
 export function map2<A, B, C>(a: Incr<A>, b: Incr<B>, fn: (x: A, y: B) => C): Incr<C> {
-  return apply(a.map((a: A) => (b: B) => fn(a, b)), b);
+  return new Map2(a._reactor, a, b, fn);
 }
 
 export function map3<A, B, C, D>(a: Incr<A>, b: Incr<B>, c: Incr<C>, fn: (a: A, b: B, c: C) => D): Incr<D> {
-  return apply<C, D>(map2<A, B, (v: C) => D>(a, b, (a, b) => c => fn(a, b, c)), c);
+  const ab = map2(a, b, (a, b) => {return {a, b}});
+  return map2(ab, c, ({a, b}, c) => fn(a, b, c));
 }
 
 export function map4<A, B, C, D, E>(
@@ -113,7 +106,8 @@ export function map4<A, B, C, D, E>(
   c: Incr<C>,
   d: Incr<D>,
   fn: (a: A, b: B, c: C, d: D) => E): Incr<E> {
-    return apply<D, E>(map3<A, B, C, (v: D) => E>(a, b, c, (a, b, c) => d => fn(a, b, c, d)), d)
+    const abc = map3(a, b, c, (a, b, c) => {return {a,b,c}});
+    return map2(abc, d, ({a,b,c}, d) => fn(a,b,c,d));
 }
 
 export function map5<A, B, C, D, E, F>(
@@ -123,7 +117,8 @@ export function map5<A, B, C, D, E, F>(
   d: Incr<D>,
   e: Incr<E>,
   fn: (a: A, b: B, c: C, d: D, e: E) => F): Incr<F> {
-    return apply<E, F>(map4<A, B, C, D, (v: E) => F>(a, b, c, d, (a, b, c, d) => e => fn(a, b, c, d, e)), e);
+    const abcd = map4(a, b, c, d, (a,b,c,d) => {return {a,b,c,d}});
+    return map2(abcd, e, ({a,b,c,d}, e) => fn(a, b, c, d, e));
 }
 
 class Obs<T> extends Incr<T> {
@@ -238,11 +233,13 @@ export class Var<T> extends Incr<T> {
 export class Reactor {
   _alloc_height: number = 0;
   _dirty: Heap<Incr<any>>;
+  _constNull: Incr<null>
 
   constructor() {
     this._dirty = new Heap((x, y) => {
       return x._scheduled_height < y._scheduled_height
     });
+    this._constNull = this.const(null);
   }
 
   const<T>(value: T): Incr<T> {
@@ -284,32 +281,36 @@ export class Reactor {
   }
 }
 
-class Map<A, B> extends Incr<B> {
-  private _input: Incr<A>;
-  private _f: (v: A) => B;
-  private _value: Optional<B> = null;
+class Map2<A, B, C> extends Incr<C> {
+  private _a: Incr<A>;
+  private _b: Incr<B>;
+  private _f: (a: A, b: B) => C;
+  private _value: Optional<C> = null;
 
-  constructor(r: Reactor, input: Incr<A>, f: (v: A) => B) {
-    super(r, input._height + 1);
-    this._input = input;
+  constructor(r: Reactor, a: Incr<A>, b: Incr<B>, f: (a: A, b: B) => C) {
+    super(r, Math.max(a._height, b._height) + 1);
+    this._a = a;
+    this._b = b;
     this._f = f;
   }
 
-  get(): B {
+  get(): C {
     return notNull(this._value);
   }
 
   _activate(): void {
-    this._input._subscribe(this);
+    this._a._subscribe(this);
+    this._b._subscribe(this);
   }
 
   _deactivate(): void {
-    this._input._unsubscribe(this);
+    this._a._unsubscribe(this);
+    this._b._unsubscribe(this);
   }
 
   _recompute(): void {
     const last = this._value;
-    const next = this._f(this._input.get());
+    const next = this._f(this._a.get(), this._b.get());
     if(last === null || last.some !== next) {
       this._value = { some: next };
       this._notify();
